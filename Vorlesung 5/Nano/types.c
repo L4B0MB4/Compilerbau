d34e7381f12554646dec65458c56568c8e357549
L4B0MB4 <metalfan65@gmail.com>
2018-04-13 10:48:13 +0200

    works

--- Vorlesung 5/Nano/types.c
+++ Vorlesung 5/Nano/types.c
@@ -1,76 +1,42 @@
-#include "types.h"
-#include <stdio.h>
 
-char* typenames[] =
+
+#include "types.h"
+
+
+char* atomic_type_names[] =
 {
    "NoType",
    "String",
    "Integer"
 };
 
-NanoType_p allocNanoType(void);
-
-TypeTable_p TypeTableAlloc()
-{
-    TypeTable_p t =TypeTableCellAlloc();
-    return t;
-}
-
-
-void TypeTableInit(TypeTable_p table)
-{
-   NanoTypeCell type;
-
-   table->type_ctr=0;
-
-   type.constructor = tc_atomic;
-   type.typeargno   = 1;
-   type.typeargs[0] = T_NoType;
-   TypeTableInsert(table, &type);
-
-   type.typeargs[0] = T_String;
-   TypeTableInsert(table, &type);
-
-   type.typeargs[0] = T_Integer;
-   TypeTableInsert(table, &type);
-}
-
-NanoType_p allocNanoType(void)
-{
-    return (NanoTypeCell*)malloc(sizeof(NanoTypeCell));
-}
 
 int TypeCmp(NanoType_p t1, NanoType_p t2)
 {
-    if(t1->constructor == t2->constructor &&
-        t1->typeargno == t2->typeargno
-    )
-    {
-        for(int i=0;i<t1->typeargno;i++)
-        {
-            if(t1->typeargs[i]!=t2->typeargs[i])
-            {
-                return 0;
-            }
-        }
-        return 1;
-    }
-    else
-        return 0;
-}
+   int res, i;
 
-
-void TypeTablePrint(FILE* out, TypeTable_p table)
-{
-   int i;
-
-   for(i=0; i<table->type_ctr; i++)
+   res = t1->constructor - t2->constructor;
+   if(res)
    {
-      fprintf(out, "%d: ", i);
-      TypePrint(out, table, i);
-      fprintf(out, "\n");
+      return res;
    }
+   res = t1->typeargno - t2->typeargno;
+   if(res)
+   {
+      return res;
+   }
+   for(i=0; i<t1->typeargno; i++)
+   {
+      res = t1->typeargs[i] - t2->typeargs[i];
+      if(res)
+      {
+         break;
+      }
+   }
+   return res;
 }
+
+
 
 void TypePrint(FILE* out, TypeTable_p table, TypeIndex type)
 {
@@ -80,10 +46,10 @@ void TypePrint(FILE* out, TypeTable_p table, TypeIndex type)
    switch(table->types[type].constructor)
    {
    case tc_atomic:
-         fprintf(out, " atomic ! %s", typenames[type]);
+         fprintf(out, "%s", atomic_type_names[type]);
          break;
    case tc_function:
-         fprintf(out, "function !(");
+         fprintf(out, "(");
          for(i=1; i < table->types[type].typeargno; i++)
          {
             fprintf(out, "%s", sep);
@@ -99,6 +65,57 @@ void TypePrint(FILE* out, TypeTable_p table, TypeIndex type)
    }
 }
 
+
+void TypeTableInit(TypeTable_p table)
+{
+   NanoTypeCell type;
+
+   table->type_ctr=0;
+
+   /* Insert dummy no-type */
+   type.constructor = tc_atomic;
+   type.typeargno   = 1;
+   type.typeargs[0] = T_NoType;
+   TypeTableInsert(table, &type);
+
+   /* Insert atomoic types - note that constructor and argno are
+    * still set from above. */
+   type.typeargs[0] = T_String;
+   TypeTableInsert(table, &type);
+
+   type.typeargs[0] = T_Integer;
+   TypeTableInsert(table, &type);
+}
+
+
+
+TypeTable_p  TypeTableAlloc()
+{
+   TypeTable_p handle;
+
+   handle = TypeTableCellAlloc();
+   TypeTableInit(handle);
+
+   return handle;
+}
+
+
+void TypeTableFree(TypeTable_p junk)
+{
+   TypeTableCellFree(junk);
+}
+
+void TypeTablePrint(FILE* out, TypeTable_p table)
+{
+   int i;
+
+   for(i=0; i<table->type_ctr; i++)
+   {
+      fprintf(out, "%d: ", i);
+      TypePrint(out, table, i);
+      fprintf(out, "\n");
+   }
+}
 
 TypeIndex TypeTableInsert(TypeTable_p table, NanoType_p type)
 {
