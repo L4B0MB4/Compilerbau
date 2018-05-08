e58f7568f50f467a940fea5d8b985c8d4d14db66
L4B0MB4 <metalfan65@gmail.com>
2018-05-08 21:07:41 +0200

    else working

--- Vorlesung 7/Nano/compile.c
+++ Vorlesung 7/Nano/compile.c
@@ -437,6 +437,15 @@ void compileCompare(char *val, AST_p ast)
     compileStringCompare(ast->child[0]->child[1]);
     fprintf(out, ")");
     switching(ast->child[1]);
+    if(ast->type==if_stmt)
+    {
+        if(ast->child[2])
+        {
+            compileBreakAndTabs();
+            fprintf(out,"else");
+            switching(ast->child[2]);
+        }
+    }
 }
 
 void compileStringCompare(AST_p ast)
